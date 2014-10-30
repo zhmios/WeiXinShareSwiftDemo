3ef48d23b05ce9fd622a991a4b47c811bb0babb8
Zhang Haiming <zhanghaiming@keegoo.com>
2014-10-30 14:15:12 +0800

    haiming

---
+++ WeiXinSwiftDemo/ThirdViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ThirdViewController.swift
+//  WeiXinSwiftDemo
+//
+//  Created by user on 14/10/30.
+//  Copyright (c) 2014年 jianguo. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
